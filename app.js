57fff97dd99abd91682b5a02329325840bf7a910
Ian Lozinski <ian.lozinski@gmail.com>
2013-04-12 00:42:55 -0400

    house cleaning

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 var express = require('express')
-  , app = express()
+, app = express()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
 
   server.listen(3000);
-  app.use('assets/', express.static('assets'));
-
+  console.log('running on 3000');
+  app.use('/assets/', express.static('assets'));
 
   app.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html');
   });
 
-  app.get('/controller', function (req, res) {
-    res.sendfile(__dirname + '/controller.html');
-  });
+app.get('/controller', function (req, res) {
+  res.sendfile(__dirname + '/controller.html');
+});
 
 io.sockets.on('connection', function (socket) {
   socket.on('buttonPress', function(button, press) {
